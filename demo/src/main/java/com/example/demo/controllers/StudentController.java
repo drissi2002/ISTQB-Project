fd6227b928fcc1bf88bc4f90d9de8592094edd4c
HtewechIheb <ihebhtewech@gmail.com>
2022-10-24 16:45:51 +0100

    Added slight modifications for performance testing

--- demo/src/main/java/com/example/demo/controllers/StudentController.java
+++ demo/src/main/java/com/example/demo/controllers/StudentController.java
@@ -1,8 +1,10 @@
 package com.example.demo.controllers;
 
+import com.example.demo.dto.CreatedDto;
 import com.example.demo.entities.Student;
 import com.example.demo.services.StudentService;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -16,16 +18,22 @@ public class StudentController {
     private final StudentService studentService;
 
     @GetMapping
+    @ResponseStatus(HttpStatus.OK)
     public List<Student> getAllStudents() {
         return studentService.getAllStudents();
     }
 
     @PostMapping
-    public void addStudent(@Valid @RequestBody Student student) {
-        studentService.addStudent(student);
+    @ResponseStatus(HttpStatus.CREATED)
+    public CreatedDto addStudent(@Valid @RequestBody Student student) {
+        Long id = studentService.addStudent(student);
+        CreatedDto createdDto = new CreatedDto(Student.class.getSimpleName(), id.toString());
+
+        return createdDto;
     }
 
     @DeleteMapping(path = "{studentId}")
+    @ResponseStatus(HttpStatus.OK)
     public void deleteStudent(
             @PathVariable("studentId") Long studentId) {
         studentService.deleteStudent(studentId);
